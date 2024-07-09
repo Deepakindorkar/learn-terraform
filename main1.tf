8682e0edea3f37d6f7187e0ee3cd4640284e4045
Deepakindorkar <157065028+Deepakindorkar@users.noreply.github.com>
2024-07-09 16:47:11 +0530

    Create main1.tf
    create ec2 file

---
+++ main1.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "ap-south-1"
+
+}
+
+ressource "aws_instance" "deepak" {
+
+ami  = "ami-0ad21ae1d0696ad58"
+instance_type = "t2.micro"
+tags = {
+Name = "deepak"
+
+}
+}
+
